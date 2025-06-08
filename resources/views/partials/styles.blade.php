314b9335f74019e9d0c50874ae05da969f643543
fullstuckdev <informatikataufik@gmail.com>
2025-06-08 23:19:47 +0700

    fix railway

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -3,10 +3,19 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
-@vite(['resources/assets/vendor/fonts/iconify/iconify.js'])
-
-<!-- Core CSS -->
-@vite(['resources/assets/vendor/scss/core.scss','resources/assets/css/demo.css', 'resources/css/app.css'])
+@if (app()->environment('local'))
+    @vite(['resources/assets/vendor/fonts/iconify/iconify.js'])
+    <!-- Core CSS -->
+    @vite(['resources/assets/vendor/scss/core.scss','resources/assets/css/demo.css', 'resources/css/app.css'])
+@else
+    <!-- Production: Load built CSS/JS from public/build/assets -->
+    <link rel="stylesheet" href="{{ asset('build/assets/core-TmpiJ-xO.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/demo-ISkCbL8g.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/page-auth-BET8THlp.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/boxicons-CGE7lhJm.css') }}">
+    <!-- Tambahkan file lain jika perlu -->
+    <script src="{{ asset('build/assets/iconify-DdHRVXAz.js') }}" defer></script>
+@endif
 
 <!-- Vendor Styles -->
 @yield('vendor-style')
