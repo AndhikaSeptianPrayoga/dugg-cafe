1988c808fac8a087c5dec7728259bf551af41779
fullstuckdev <informatikataufik@gmail.com>
2025-06-08 23:27:15 +0700

    change to secure assets

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -9,12 +9,12 @@
     @vite(['resources/assets/vendor/scss/core.scss','resources/assets/css/demo.css', 'resources/css/app.css'])
 @else
     <!-- Production: Load built CSS/JS from public/build/assets -->
-    <link rel="stylesheet" href="{{ asset('build/assets/core-TmpiJ-xO.css') }}">
-    <link rel="stylesheet" href="{{ asset('build/assets/demo-ISkCbL8g.css') }}">
-    <link rel="stylesheet" href="{{ asset('build/assets/page-auth-BET8THlp.css') }}">
-    <link rel="stylesheet" href="{{ asset('build/assets/boxicons-CGE7lhJm.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/core-TmpiJ-xO.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/demo-ISkCbL8g.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/page-auth-BET8THlp.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/boxicons-CGE7lhJm.css') }}">
     <!-- Tambahkan file lain jika perlu -->
-    <script src="{{ asset('build/assets/iconify-DdHRVXAz.js') }}" defer></script>
+    <script src="{{ secure_asset('build/assets/iconify-DdHRVXAz.js') }}" defer></script>
 @endif
 
 <!-- Vendor Styles -->
